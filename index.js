aedc9aa65f08dc3a4b968fba380f3afac61ecf67
Naman Chandok <42838783+NamanChandok@users.noreply.github.com>
2020-07-26 19:34:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// LOADER
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("load").style.display = "none";
+    document.getElementById("mainbod").style.display = "block";
+}
+
+// DARK MODE
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+    // SAVE PREFERENCE TO LOCAL STORAGE
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+    // CHECK LOCAL STORAGE FOR PREFERENCE
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+
+    // SIDE-NAV
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
